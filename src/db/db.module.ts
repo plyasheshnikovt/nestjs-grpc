70507f60376c4a8774651b84c8da552f58f57fa1
Timofey <plyasheshnikovt@gmail.com>
2022-04-09 12:21:30 +0300

    fixes

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { databaseProvider } from './db.provider';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [databaseProvider],
   exports: [databaseProvider],
-  imports: [ConfigModule.forRoot()],
+  imports: [],
 })
 export class DatabaseModule {}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../db/db.module';
 import { FlickrModule } from './flickr/flickr.module';
 import { PhotoModule } from './photo/photo.module';
+import { ConfigModule } from '../config';
 
 @Module({
-  imports: [DatabaseModule, FlickrModule, PhotoModule],
+  imports: [ConfigModule, DatabaseModule, FlickrModule, PhotoModule],
   controllers: [],
   providers: [],
 })
